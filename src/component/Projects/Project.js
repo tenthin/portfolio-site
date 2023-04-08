f248ffd9093d532cddde74009ea040535767870e
Tenzin <tenthinten@gmail.com>
2023-04-08 15:38:41 +0530

    link my tsewangs shop project

--- src/component/Projects/Project.js
+++ src/component/Projects/Project.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Project.scss";
 import { projects } from "./ProjectData";
+import { NavLink } from "react-router-dom";
 
 function Project() {
+  const handleOpenLink = (link) => {
+    // Open the link in a new window
+    window.open(link, "_blank");
+  };
   return (
     <div className="my-proj" id="projects">
       <div className="project-title">MY PROJECTS</div>
@@ -19,7 +24,9 @@ function Project() {
                 <p>{project.summary}</p>
               </div>
               ` `{/* <NavLink to={`/fullproject/${project.id}`}> */}
-              <button>FULL PROJECT</button>
+              <button onClick={() => handleOpenLink(project.link)}>
+                FULL PROJECT
+              </button>
               {/* </NavLink> */}
             </div>
           </div>
--- src/component/Projects/ProjectData.js
+++ src/component/Projects/ProjectData.js
@@ -3,6 +3,7 @@ export const projects = [
     id: 1,
     image: "/img/project1.png",
     name: "Tsewangs Shop",
+    link: "https://tsewangshop.netlify.app/",
     summary:
       "A site to display all the products that are sold in batches. Customer can check the products through the list and contact the seller through contact number and negotiate the price.",
   },
@@ -10,6 +11,7 @@ export const projects = [
     id: 2,
     image: "/img/project2.png",
     name: "Shopping Cart",
+    link: "",
     summary:
       "A site to display all the products that are sold in batches. Customer can check the products through the list and contact the seller through contact number and negotiate the price.",
   },
